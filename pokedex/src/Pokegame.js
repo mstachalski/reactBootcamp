5283f6548b86f8e7e9d94d265106e6268d609c9d
Max Stachalski <max.stachalski@stud.tu-darmstadt.de>
2019-12-17 03:49:56 +0100

    [refactor] Change shuffle algorithm to Fisher-Yates

--- pokedex/src/Pokegame.js
+++ pokedex/src/Pokegame.js
@@ -25,9 +25,10 @@ class Pokegame extends React.Component {
     }
 
     shufflePokemon() {
-        pokemon.sort(() =>
-            Math.random() - 0.5
-        )
+        for (let i = pokemon.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [pokemon[i], pokemon[j]] = [pokemon[j], pokemon[i]];
+        }
     }
 
     assignHands() {
